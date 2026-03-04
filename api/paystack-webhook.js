98e7f405f714bbaf1918e833403fea2f4428284b
clarity-web-growth <louisandoh63@gmail.com>
2026-03-04 16:37:51 +0000

    Push to GitHub

--- api/paystack-webhook.js
+++ api/paystack-webhook.js
@@ -1,6 +1,6 @@
-import crypto from "crypto";
+const crypto = require("crypto");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
